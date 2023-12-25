867a7fdd0311fc91b56165a6708e10383306da3f
leechi <leechi2228@gmail.com>
2023-12-25 10:19:36 +0900

    style(calculator): Add calculator UI

--- calculator/App.js
+++ calculator/App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import Calculator from "./src/Calculator";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Calculator />
-    </View>
+    </SafeAreaView>
   );
 }
 
--- calculator/src/Calculator.js
+++ calculator/src/Calculator.js
@@ -1,9 +1,32 @@
 import React from "react";
-import { View } from "react-native";
+import { View, TouchableOpacity, Text } from "react-native";
 
 // Button type: 'reset' | 'operator' | 'num'
 const Button = ({ text, onPress, flex, type }) => {
-  return <TouchableOpacity style={{}}></TouchableOpacity>;
+  const backgroundColor =
+    type === "reset"
+      ? COLOR.RESET
+      : type === "operator"
+      ? COLOR.OPERATOR
+      : type === "num"
+      ? COLOR.NUM
+      : "transparent";
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={{
+        flex,
+        backgroundColor,
+        justifyContent: "center",
+        alignItems: "center",
+        height: 50,
+        borderWidth: 0.2,
+        borderColor: "black",
+      }}
+    >
+      <Text style={{ color: "white", fontSize: 25 }}>{text}</Text>
+    </TouchableOpacity>
+  );
 };
 
 const COLOR = {
@@ -15,18 +38,42 @@ const COLOR = {
 
 export default () => {
   return (
-    <View>
+    <View style={{ flex: 1, width: 250 }}>
       {/* 결과 */}
 
       {/* [AC ~ /] */}
-
+      <View style={{ flexDirection: "row" }}>
+        <Button type="reset" text="AC" onPress={() => null} flex={3} />
+        <Button type="operator" text="/" onPress={() => null} flex={1} />
+      </View>
       {/* [7 ~ x] */}
+      <View style={{ flexDirection: "row", width: "100%" }}>
+        <Button type="num" text="7" onPress={() => null} flex={1} />
+        <Button type="num" text="8" onPress={() => null} flex={1} />
+        <Button type="num" text="9" onPress={() => null} flex={1} />
+        <Button type="operator" text="X" onPress={() => null} flex={1} />
+      </View>
 
       {/* [4 ~ -] */}
+      <View style={{ flexDirection: "row", width: "100%" }}>
+        <Button type="num" text="4" onPress={() => null} flex={1} />
+        <Button type="num" text="5" onPress={() => null} flex={1} />
+        <Button type="num" text="6" onPress={() => null} flex={1} />
+        <Button type="operator" text="-" onPress={() => null} flex={1} />
+      </View>
 
       {/* [1 ~ +] */}
-
+      <View style={{ flexDirection: "row", width: "100%" }}>
+        <Button type="num" text="1" onPress={() => null} flex={1} />
+        <Button type="num" text="2" onPress={() => null} flex={1} />
+        <Button type="num" text="3" onPress={() => null} flex={1} />
+        <Button type="operator" text="+" onPress={() => null} flex={1} />
+      </View>
       {/* [0 ~ =] */}
+      <View style={{ flexDirection: "row", width: "100%" }}>
+        <Button type="num" text="0" onPress={() => null} flex={3} />
+        <Button type="operator" text="=" onPress={() => null} flex={1} />
+      </View>
     </View>
   );
 };
