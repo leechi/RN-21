ef92456488c4eb5cac0c09ae95191c151d3ac1d0
leechi <leechi2228@gmail.com>
2023-12-26 22:17:54 +0900

    feat: Add utils

--- todo/App.js
+++ todo/App.js
@@ -1,7 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import dayjs from "dayjs";
+import { StyleSheet, Text, View } from "react-native";
+import { getCalendarColumns } from "./src/util";
+import { useEffect } from "react";
 
 export default function App() {
+  const now = dayjs();
+  const columns = getCalendarColumns(now);
+
+  useEffect(() => {
+    console.log(columns);
+  });
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
